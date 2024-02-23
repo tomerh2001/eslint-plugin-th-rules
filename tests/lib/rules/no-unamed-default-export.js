cfda4c16bbba11b281a45deca8ca8742ecab23e9
Tomer Horowitz <tomerh2001@gmail.com>
2024-02-23 12:44:45 +0200

    feat: Added a rule to convert unnamed default exports to named default exports based on the file name

---
+++ tests/lib/rules/no-unamed-default-export.js
@@ -0,0 +1,56 @@
+import {RuleTester} from 'eslint';
+import rule from '../../../lib/rules/no-unamed-default-export.js';
+
+const ruleTester = new RuleTester({
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+});
+
+ruleTester.run('no-unamed-default-export', rule, {
+	valid: [
+		// Already named exports should pass
+		'const a = 1; export default a;',
+		'export default function foo() {}',
+		'export default class Foo {}',
+	],
+	invalid: [
+		// Function exports without a name
+		{
+			code: 'export default function () {}',
+			output: 'const Input = function () {};\nexport default Input;',
+			errors: [{message: 'Unnamed default export should be named based on the file name.'}],
+		},
+		// Class exports without a name
+		{
+			code: 'export default class {}',
+			output: 'const Input = class {};\nexport default Input;',
+			errors: [{message: 'Unnamed default export should be named based on the file name.'}],
+		},
+		// Object literal exports
+		{
+			code: 'export default {}',
+			output: 'const Input = {};\nexport default Input;',
+			errors: [{message: 'Unnamed default export should be named based on the file name.'}],
+		},
+		// Primitive value exports
+		{
+			code: 'export default 42',
+			output: 'const Input = 42;\nexport default Input;',
+			errors: [{message: 'Unnamed default export should be named based on the file name.'}],
+		},
+		// Null exports
+		{
+			code: 'export default null',
+			output: 'const Input = null;\nexport default Input;',
+			errors: [{message: 'Unnamed default export should be named based on the file name.'}],
+		},
+		// Arrow function exports
+		{
+			code: 'export default () => {}',
+			output: 'const Input = () => {};\nexport default Input;',
+			errors: [{message: 'Unnamed default export should be named based on the file name.'}],
+		},
+	],
+});
