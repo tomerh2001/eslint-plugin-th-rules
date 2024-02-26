45b62978da758e4ebbf62780e74595393c38800b
Tomer Horowitz <tomerh2001@gmail.com>
2024-02-26 23:36:22 +0200

    fix: Add ESLint configurations for JSDoc and React

--- src/index.js
+++ src/index.js
@@ -9,28 +9,77 @@
 const requireIndex = require('requireindex');
 
 /**
- * ESLint rules configuration.
- * @type {Object.<string, string>}
- */
-const rules = {
-	'th-rules/no-destructuring': 'error',
-	'th-rules/no-default-export': 'error',
-};
-
-/**
  * Represents a basic configuration object.
- * @typedef {Object} BasicConfig
+ *
+ * @type {Object}
  * @property {string[]} plugins - The list of plugins.
- * @property {Object} rules - The rules object.
- * @type {BasicConfig}
+ * @property {string[]} extends - The list of extended configurations.
+ * @property {Object} rules - The rules configuration.
+ * @property {Object} envs - The environment configuration.
  */
 const basic = {
 	plugins: ['th-rules'],
-	rules,
+	extends: [],
+	rules: {
+		'th-rules/no-destructuring': 'error',
+		'th-rules/no-default-export': 'error',
+	},
+	envs: {},
 };
 
+/**
+ * Represents the recommended ESLint configuration.
+ * @type {Object}
+ */
 const recommended = {
 	...basic,
+	envs: {
+		node: true,
+		ES2024: true,
+		jest: true,
+	},
+	rules: {
+		...basic.rules,
+		'unicorn/filename-case': 'off',
+		'unicorn/prefer-module': 'warn',
+		'import/extensions': 'off',
+		camelcase: 'warn',
+	},
+};
+
+/**
+ * Represents an object containing all the configurations.
+ * @type {Object}
+ */
+const all = {
+	...recommended,
+	plugins: [...recommended.plugins, 'jsdoc'],
+	extends: [...recommended.extends, 'plugin:jsdoc/recommended-error'],
+};
+
+/**
+ * Represents an object containing all React-related configurations.
+ * @typedef {Object} allReact
+ * @property {Object} envs - The environment configurations.
+ * @property {boolean} envs.browser - Indicates if the browser environment is enabled.
+ * @property {Object} rules - The rule configurations.
+ * @property {string} rules['react/no-array-index-key'] - The configuration for the 'react/no-array-index-key' rule.
+ * @property {string[]} extends - The list of extended configurations.
+ */
+const allReact = {
+	...all,
+	envs: {...all.envs, node: false, browser: true},
+	rules: {...all.rules, 'react/no-array-index-key': 'off'},
+	extends: [...all.extends, 'xo-react'],
+};
+
+/**
+ * Object representing all React Native configurations.
+ * @type {Object}
+ */
+const allReactNative = {
+	...allReact,
+	extends: [...allReact.extends, 'plugin:react-native/all'],
 };
 
 module.exports = {
@@ -38,5 +87,8 @@ module.exports = {
 	configs: {
 		basic,
 		recommended,
+		all,
+		'all-react': allReact,
+		'all-react-native': allReactNative,
 	},
 };
