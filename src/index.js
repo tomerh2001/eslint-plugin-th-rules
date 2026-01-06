0af8a5e49026af3145924363949cdb2978886dbf
Tomer Horowitz <tomerh@realcommerce.co.il>
2026-01-06 17:06:42 +0200

    feat(eslint): :boom: migrated to ESLint flat configs
    Migrated to ESLint flat config

--- src/index.js
+++ src/index.js
@@ -2,60 +2,83 @@
 /* eslint-disable unicorn/prefer-module */
 
 'use strict';
-const requireIndex = require('requireindex');
 
-const configs = {
-	recommended: {
-		plugins: [
-			'th-rules',
-			'sonarjs',
-		],
-		extends: [
-			'plugin:sonarjs/recommended-legacy',
-			'plugin:security/recommended-legacy',
-		],
-		rules: {
-			'th-rules/no-destructuring': 'error',
-			'th-rules/no-default-export': 'error',
-			'th-rules/no-comments': 'error',
-			'th-rules/top-level-functions': 'error',
-			'th-rules/schemas-in-schemas-file': 'error',
-			'th-rules/types-in-dts': 'error',
-			'unicorn/prefer-module': 'warn',
-			'unicorn/filename-case': 'off',
-			'unicorn/no-array-callback-reference': 'off',
-			'import/extensions': 'off',
-			'unicorn/no-static-only-class': 'off',
-			'unicorn/no-await-expression-member': 'off',
-			'new-cap': 'off',
-			'no-await-in-loop': 'off',
-			'n/file-extension-in-import': 'off',
-			'import/no-cycle': 'off',
-			camelcase: 'warn',
-			'sonarjs/mouse-events-a11y': 'off',
-			'sonarjs/no-unstable-nested-components': 'off',
-			'unicorn/prefer-global-this': 'off',
-			'unicorn/no-thenable': 'off',
-			'sonarjs/no-clear-text-protocols': 'off',
+const requireIndex = require('requireindex');
+const globals = require('globals');
+const sonarjs = require('eslint-plugin-sonarjs');
+const security = require('eslint-plugin-security');
+const reactPlugin = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const tseslint = require('typescript-eslint');
+
+const plugin = {
+	rules: requireIndex(`${__dirname}/rules`),
+	configs: {},
+};
+
+const baseRecommended = {
+	plugins: {
+		'th-rules': plugin,
+		security,
+		react: reactPlugin,
+		'react-hooks': reactHooks,
+	},
+	languageOptions: {
+		ecmaVersion: 2024,
+		globals: {
+			...globals.node,
+			...globals.es2024,
+			...globals.jest,
 		},
-		env: {
-			node: true,
-			es2024: true,
-			jest: true,
-		},
+	},
+	settings: {
+		react: {version: 'detect'},
+	},
+	rules: {
+		'th-rules/no-destructuring': 'error',
+		'th-rules/no-default-export': 'error',
+		'th-rules/no-comments': 'error',
+		'th-rules/top-level-functions': 'error',
+		'th-rules/schemas-in-schemas-file': 'error',
+		'th-rules/types-in-dts': 'error',
+
+		'unicorn/prefer-module': 'warn',
+		'unicorn/filename-case': 'off',
+		'unicorn/no-array-callback-reference': 'off',
+		'import/extensions': 'off',
+		'unicorn/no-static-only-class': 'off',
+		'unicorn/no-await-expression-member': 'off',
+		'new-cap': 'off',
+		'no-await-in-loop': 'off',
+		'n/file-extension-in-import': 'off',
+		'import/no-cycle': 'off',
+		camelcase: 'warn',
+
+		'sonarjs/mouse-events-a11y': 'off',
+		'sonarjs/no-unstable-nested-components': 'off',
+		'unicorn/prefer-global-this': 'off',
+		'unicorn/no-thenable': 'off',
+		'sonarjs/no-clear-text-protocols': 'off',
 	},
 };
 
-for (const configName of Object.keys(configs)) {
-	configs[`${configName}-typescript`] = {
-		...configs[configName],
-		extends: [
-			'plugin:@typescript-eslint/strict-type-checked',
-			'plugin:@typescript-eslint/stylistic-type-checked',
-			...configs[configName].extends,
-		],
+plugin.configs.recommended = [
+	sonarjs.configs.recommended,
+	security.configs.recommended,
+	baseRecommended,
+];
+
+plugin.configs['recommended-typescript'] = [
+	...plugin.configs.recommended,
+	tseslint.configs.strictTypeChecked,
+	tseslint.configs.stylisticTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+			},
+		},
 		rules: {
-			...configs[configName].rules,
 			'@typescript-eslint/naming-convention': 'warn',
 			'@typescript-eslint/consistent-type-definitions': 'off',
 			'@typescript-eslint/no-extraneous-class': 'off',
@@ -67,22 +90,18 @@ for (const configName of Object.keys(configs)) {
 			'@typescript-eslint/no-unsafe-return': 'off',
 			'@typescript-eslint/no-unsafe-argument': 'off',
 		},
-	};
-	configs[`${configName}-react`] = {
-		...configs[configName],
-		extends: [
-			'plugin:react/recommended',
-			'plugin:react-hooks/recommended',
-			...configs[configName].extends,
-		],
+	},
+];
+
+plugin.configs['recommended-react'] = [
+	...plugin.configs.recommended,
+	reactPlugin.configs.flat.recommended,
+	reactHooks.configs['recommended-latest'] ?? reactHooks.configs.recommended,
+	{
 		rules: {
-			...configs[configName].rules,
 			'n/prefer-global/process': 'off',
 		},
-	};
-}
+	},
+];
 
-module.exports = {
-	rules: requireIndex(`${__dirname}/rules`),
-	configs,
-};
+module.exports = plugin;
